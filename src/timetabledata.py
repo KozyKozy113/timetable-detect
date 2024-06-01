760ea267d55488f65c79c544b1d3f28536c6e5dc
KozyKozy113 <kutatyu.sayumimi1633@gmail.com>
2024-06-02 02:44:08 +0900

    OCR結果JSONの不備への対応

--- src/timetabledata.py
+++ src/timetabledata.py
@@ -13,19 +13,20 @@ def json_to_df(json_data):
             live_stage_to = ""
         
         # 特典会の情報を処理
-        for meeting in item['特典会']:
-            try:
-                meeting_from = meeting['from']
-            except KeyError:
-                meeting_from = ""
-            try:
-                meeting_to = meeting['to']
-            except KeyError:
-                meeting_to = ""
-            try:
-                booth = meeting['ブース']
-            except KeyError:
-                booth = ""
+        try:
+            for meeting in item['特典会']:
+                try:
+                    meeting_from = meeting['from']
+                except KeyError:
+                    meeting_from = ""
+                try:
+                    meeting_to = meeting['to']
+                except KeyError:
+                    meeting_to = ""
+                try:
+                    booth = meeting['ブース']
+                except KeyError:
+                    booth = ""
             
             # DataFrameに行を追加
             df_timetable.append({
@@ -36,7 +37,17 @@ def json_to_df(json_data):
                 '特典会_to': meeting_to,
                 'ブース': booth
             })
+        except KeyError:
+            df_timetable.append({
+                'グループ名': item['グループ名'],
+                'ライブステージ_from': live_stage_from,
+                'ライブステージ_to': live_stage_to,
+                '特典会_from': "",
+                '特典会_to': "",
+                'ブース': ""
+            })
 
     df_timetable = pd.DataFrame(df_timetable,columns=['グループ名', 'ライブステージ_from', 'ライブステージ_to', '特典会_from', '特典会_to', 'ブース'])
+    df_timetable.sort_values(by="ライブステージ_from",inplace=True)
 
     return df_timetable
